5598300978f9890a184e61ab5177ff88e537006c
Tariq-Monowar <101199109+Tariq-Monowar@users.noreply.github.com>
2023-01-07 12:29:15 +0600

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,27 +1,3 @@
-// const { getAllUser,
-//         createUser,
-//         updateUsers,
-//         deleteUser,
-//         getOneUser,
-//         singin 
-//     } = require('../controller/user.controller')
-
-// const router = require('express').Router()
-
-// router.get('/',getAllUser)
-// router.get('/:id',getOneUser)
-// router.post('/',createUser)
-// router.patch('/:id',updateUsers)
-// router.delete('/:id',deleteUser)
-
-
-// router.post('/login',singin)
-
-
-
-// module.exports = router
-
-
 const express = require('express');
 const router = express.Router();
 
@@ -34,12 +10,11 @@ const {
    singin 
 } = require('../controller/user.controller');
 
-router.get('/', getAllUser);
-router.get('/:id', getOneUser);
-router.post('/', createUser);
-router.patch('/:id', updateUsers);
-router.delete('/:id', deleteUser);
+router.route("/").get(getAllUser).post(createUser);
+router.route("/:id").get(getOneUser).patch(updateUsers).delete(deleteUser);
 
 router.post('/login', singin);
 
+
 module.exports = router;
+
